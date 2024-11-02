3d7ab4d0946646c3c145052c4af7e0af0d838a61
ereny-svg <erenymahrous123@gmail.com>
2024-11-02 17:15:50 +0200

    Finish Home Screen Ui

---
+++ lib/taps/tasks/task_tap.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TasksTap extends StatelessWidget {
+  const TasksTap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'tasks',
+      ),
+    );
+  }
+}
