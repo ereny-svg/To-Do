be5d8937c966c6f1589a999ee7c6b4dfb8718215
ereny-svg <erenymahrous123@gmail.com>
2024-11-21 02:17:25 +0200

    changes of development

--- lib/taps/tasks/update_task.dart
+++ lib/taps/tasks/update_task.dart
@@ -1,14 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/app_theme.dart';
 import 'package:todo/firebase_functions.dart';
-import 'package:todo/taps/setting/setting_provider.dart';
+import 'package:todo/models/taskmodel.dart';
 import 'package:todo/taps/tasks_provider.dart';
 import 'package:todo/widgets/default_elevated_button.dart';
 import 'package:todo/widgets/default_text_form_field.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class UpdateTask extends StatefulWidget {
   static const String routename = '/update';
@@ -27,7 +27,6 @@ class _UpdateTaskState extends State<UpdateTask> {
   DateFormat dateFormat = DateFormat('dd/MM/yyyy');
   @override
   Widget build(BuildContext context) {
-    SettingProvider settingProvider = Provider.of<SettingProvider>(context);
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
     if (args != null) {
@@ -42,48 +41,45 @@ class _UpdateTaskState extends State<UpdateTask> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          AppLocalizations.of(context)!.todolist,
+          'To Do List',
           style: theme.textTheme.bodySmall,
         ),
         centerTitle: false,
         foregroundColor: AppTheme.white,
         backgroundColor: theme.primaryColor,
       ),
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Container(
-              height: height * 0.07,
-              color: theme.primaryColor,
-              width: double.infinity,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-              child: Container(
-                width: 365,
-                height: 688,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: settingProvider.themeMode == ThemeMode.light
-                      ? AppTheme.white
-                      : AppTheme.darkblue,
-                ),
-                child: Form(
-                  key: formKey,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
-                          child: Text(
-                            AppLocalizations.of(context)!.updatetask,
-                            style: Theme.of(context).textTheme.titleMedium,
-                          ),
+      body: Stack(
+        children: [
+          Container(
+            height: height * 0.07,
+            color: theme.primaryColor,
+            width: double.infinity,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+            child: Container(
+              width: 365,
+              height: 688,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: AppTheme.white,
+              ),
+              child: Form(
+                key: formKey,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Text(
+                          'Edit Task',
+                          style: Theme.of(context).textTheme.titleMedium,
                         ),
-                        const SizedBox(
-                          height: 52,
-                        ),
-                        DefaultTextFormField(
+                      ),
+                      const SizedBox(
+                        height: 52,
+                      ),
+                      DefaultTextFormField(
                           validator: (value) {
                             if (value == null || value.trim().isEmpty) {
                               return 'Please enter task title';
@@ -91,115 +87,84 @@ class _UpdateTaskState extends State<UpdateTask> {
                             return null;
                           },
                           controller: titlecontroller,
-                          hinttext: 'This is title',
-                          textStyle: Theme.of(context)
-                              .textTheme
-                              .titleMedium!
-                              .copyWith(
-                                color:
-                                    settingProvider.themeMode == ThemeMode.light
-                                        ? AppTheme.black
-                                        : AppTheme.white,
-                              ),
-                        ),
-                        const SizedBox(
-                          height: 31,
-                        ),
-                        DefaultTextFormField(
-                            validator: (value) {
-                              if (value == null || value.trim().isEmpty) {
-                                return 'Please enter task Description';
-                              }
-                              return null;
-                            },
-                            controller: descriptioncontroller,
-                            textStyle: Theme.of(context)
-                                .textTheme
-                                .titleMedium!
-                                .copyWith(
-                                  color: settingProvider.themeMode ==
-                                          ThemeMode.light
-                                      ? AppTheme.black
-                                      : AppTheme.white,
-                                ),
-                            hinttext: 'Task details'),
-                        const SizedBox(
-                          height: 31,
-                        ),
-                        Text(
-                          AppLocalizations.of(context)!.selectdate,
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        Center(
-                            child: GestureDetector(
-                                onTap: () async {
-                                  DateTime? datetime = await showDatePicker(
-                                      initialEntryMode:
-                                          DatePickerEntryMode.calendarOnly,
-                                      context: context,
-                                      initialDate: selecteddate,
-                                      firstDate: DateTime.now(),
-                                      lastDate: DateTime.now()
-                                          .add(Duration(days: 365)));
-                                  if (datetime != null &&
-                                      selecteddate != datetime) {
-                                    selecteddate = datetime;
-        
-                                    setState(() {});
-                                  }
-                                },
-                                child: Text(
-                                  dateFormat.format(selecteddate),
-                                  style: TextStyle(
-                                    color: settingProvider.themeMode ==
-                                            ThemeMode.light
-                                        ? AppTheme.black
-                                        : AppTheme.white,
-                                  ),
-                                ))),
-                        const SizedBox(
-                          height: 115,
-                        ),
-                        DefaultElevatedButton(
-                            text: AppLocalizations.of(context)!.savechanges,
-                            onPressed: () {
-                              if (formKey.currentState!.validate()) {
-                                FirebaseFunctions.UpdateTaskFromFirestore(
-                                        taskId,
-                                        titlecontroller.text,
-                                        descriptioncontroller.text,
-                                        selecteddate)
-                                    .timeout(
-                                  Duration(microseconds: 100),
-                                  onTimeout: () => Provider.of<TasksProvider>(
-                                          context,
-                                          listen: false)
-                                      .getTasks(),
-                                )
-                                    .catchError((error) {
-                                  Fluttertoast.showToast(
-                                      msg: "Something went wrong",
-                                      toastLength: Toast.LENGTH_LONG,
-                                      gravity: ToastGravity.BOTTOM,
-                                      timeInSecForIosWeb: 5,
-                                      backgroundColor: Colors.red,
-                                      textColor: Colors.white,
-                                      fontSize: 16.0);
-                                });
-        
+                          hinttext: 'This is title'),
+                      const SizedBox(
+                        height: 31,
+                      ),
+                      DefaultTextFormField(
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Please enter task Description';
+                            }
+                            return null;
+                          },
+                          controller: descriptioncontroller,
+                          hinttext: 'Task details'),
+                      const SizedBox(
+                        height: 31,
+                      ),
+                      Text(
+                        'Select Date',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Center(
+                          child: GestureDetector(
+                              onTap: () async {
+                                DateTime? datetime = await showDatePicker(
+                                    initialEntryMode:
+                                        DatePickerEntryMode.calendarOnly,
+                                    context: context,
+                                    initialDate: selecteddate,
+                                    firstDate: DateTime.now(),
+                                    lastDate: DateTime.now()
+                                        .add(Duration(days: 365)));
+
+                                selecteddate = datetime !;
                                 setState(() {});
-                                Navigator.of(context).pop();
-                              }
-                            })
-                      ]),
-                ),
+                              },
+                              child: Text(dateFormat.format(selecteddate)))),
+                      const SizedBox(
+                        height: 115,
+                      ),
+                      DefaultElevatedButton(
+                          text: 'Save Changes',
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              FirebaseFunctions.UpdateTaskFromFirestore(
+                                      taskId,
+                                      titlecontroller.text,
+                                      descriptioncontroller.text,
+                                      selecteddate)
+                                  .timeout(
+                                Duration(microseconds: 100),
+                                onTimeout: () => Provider.of<TasksProvider>(
+                                        context,
+                                        listen: false)
+                                    .getTasks(),
+                              )
+                                  .catchError((error) {
+                                Fluttertoast.showToast(
+                                    msg: "Something went wrong",
+                                    toastLength: Toast.LENGTH_LONG,
+                                    gravity: ToastGravity.BOTTOM,
+                                    timeInSecForIosWeb: 5,
+                                    backgroundColor: Colors.red,
+                                    textColor: Colors.white,
+                                    fontSize: 16.0);
+                              });
+
+                              setState(() {});
+                              Navigator.of(context).pop();
+                            }
+                          })
+                    ]),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
