4f8aeee7f324577bdf48c076380b5b454bb4f45c
ereny-svg <erenymahrous123@gmail.com>
2024-12-02 23:26:31 +0200

    add singlechildscrollview to update task

--- lib/taps/tasks/update_task.dart
+++ lib/taps/tasks/update_task.dart
@@ -18,6 +18,7 @@ class UpdateTask extends StatefulWidget {
 }
 
 class _UpdateTaskState extends State<UpdateTask> {
+  
   late String taskId;
   var formKey = GlobalKey<FormState>();
   TextEditingController titlecontroller = TextEditingController();
@@ -27,10 +28,10 @@ class _UpdateTaskState extends State<UpdateTask> {
   @override
   Widget build(BuildContext context) {
     TasksProvider tasksProvider = Provider.of<TasksProvider>(context);
-    final args=ModalRoute.of(context)?.settings.arguments as TaskModel;
-    titlecontroller.text=args.title;
-    descriptioncontroller.text=args.description;
-    taskId=args.id;
+    final args = ModalRoute.of(context)?.settings.arguments as TaskModel;
+    titlecontroller.text = args.title;
+    descriptioncontroller.text = args.description;
+    taskId = args.id;
     double height = MediaQuery.of(context).size.height;
     ThemeData theme = Theme.of(context);
     return Scaffold(
@@ -43,130 +44,133 @@ class _UpdateTaskState extends State<UpdateTask> {
         foregroundColor: AppTheme.white,
         backgroundColor: theme.primaryColor,
       ),
-      body: Stack(
-        children: [
-          Container(
-            height: height * 0.07,
-            color: theme.primaryColor,
-            width: double.infinity,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-            child: Container(
-              width: 365,
-              height: 688,
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: AppTheme.white,
-              ),
-              child: Form(
-                key: formKey,
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Center(
-                        child: Text(
-                          'Edit Task',
-                          style: Theme.of(context).textTheme.titleMedium,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              height: height * 0.07,
+              color: theme.primaryColor,
+              width: double.infinity,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+              child: Container(
+                width: 365,
+                height: 688,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: AppTheme.white,
+                ),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: Text(
+                            'Edit Task',
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 52,
-                      ),
-                      DefaultTextFormField(
-                          validator: (value) {
-                            if (value == null || value.trim().isEmpty) {
-                              return 'Please enter task title';
-                            }
-                            return null;
-                          },
-                          controller: titlecontroller,
-                          hinttext: 'This is title'),
-                      const SizedBox(
-                        height: 31,
-                      ),
-                      DefaultTextFormField(
-                          validator: (value) {
-                            if (value == null || value.trim().isEmpty) {
-                              return 'Please enter task Description';
-                            }
-                            return null;
-                          },
-                          controller: descriptioncontroller,
-                          hinttext: 'Task details'),
-                      const SizedBox(
-                        height: 31,
-                      ),
-                      Text(
-                        'Select Date',
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      Center(
-                          child: GestureDetector(
-                              onTap: () async {
-                                DateTime? datetime = await showDatePicker(
-                                    initialEntryMode:
-                                        DatePickerEntryMode.calendarOnly,
-                                    context: context,
-                                    initialDate: selecteddate,
-                                    firstDate: DateTime.now(),
-                                    lastDate: DateTime.now()
-                                        .add(Duration(days: 365)));
-                                if (datetime != null &&
-                                    selecteddate != datetime) {
-                                   selecteddate = datetime;
-                                }
-                              },
-                              child: Text(dateFormat.format(selecteddate)))),
-                      const SizedBox(
-                        height: 115,
-                      ),
-                      DefaultElevatedButton(
-                          text: 'Save Changes',
-                          onPressed: () {
-                            if (formKey.currentState!.validate()) {
-                              TaskModel task = TaskModel(
-                                  id: taskId,
-                                  title: titlecontroller.text,
-                                  description: descriptioncontroller.text,
-                                  date: selecteddate);
-                              tasksProvider.updateTask(task).timeout(
-                                Duration(milliseconds: 100),
-                                onTimeout: () {
-                                  Navigator.of(context).pop();
-                                  Provider.of<TasksProvider>(context,
-                                          listen: false)
-                                      .getTasks();
+                        const SizedBox(
+                          height: 52,
+                        ),
+                        DefaultTextFormField(
+                            validator: (value) {
+                              if (value == null || value.trim().isEmpty) {
+                                return 'Please enter task title';
+                              }
+                              return null;
+                            },
+                            controller: titlecontroller,
+                            hinttext: 'This is title'),
+                        const SizedBox(
+                          height: 31,
+                        ),
+                        DefaultTextFormField(
+                            validator: (value) {
+                              if (value == null || value.trim().isEmpty) {
+                                return 'Please enter task Description';
+                              }
+                              return null;
+                            },
+                            controller: descriptioncontroller,
+                            hinttext: 'Task details'),
+                        const SizedBox(
+                          height: 31,
+                        ),
+                        Text(
+                          'Select Date',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Center(
+                            child: GestureDetector(
+                                onTap: () async {
+                                  DateTime? datetime = await showDatePicker(
+                                      initialEntryMode:
+                                          DatePickerEntryMode.calendarOnly,
+                                      context: context,
+                                      initialDate: selecteddate,
+                                      firstDate: DateTime.now(),
+                                      lastDate: DateTime.now()
+                                          .add(Duration(days: 365)));
+                                  if (datetime != null &&
+                                      selecteddate != datetime) {
+                                    selecteddate = datetime;
+                                   
+                                  }
+                                },
+                                child: Text(dateFormat.format(selecteddate)))),
+                        const SizedBox(
+                          height: 115,
+                        ),
+                        DefaultElevatedButton(
+                            text: 'Save Changes',
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {
+                                TaskModel task = TaskModel(
+                                    id: taskId,
+                                    title: titlecontroller.text,
+                                    description: descriptioncontroller.text,
+                                    date: selecteddate);
+                                tasksProvider.updateTask(task).timeout(
+                                  Duration(milliseconds: 100),
+                                  onTimeout: () {
+                                    Navigator.of(context).pop();
+                                    Provider.of<TasksProvider>(context,
+                                            listen: false)
+                                        .getTasks();
+                                    Fluttertoast.showToast(
+                                        msg: "Task added successfully",
+                                        toastLength: Toast.LENGTH_LONG,
+                                        gravity: ToastGravity.BOTTOM,
+                                        timeInSecForIosWeb: 5,
+                                        backgroundColor: Colors.green,
+                                        textColor: Colors.white,
+                                        fontSize: 16.0);
+                                  },
+                                ).catchError((error) {
                                   Fluttertoast.showToast(
-                                      msg: "Task added successfully",
+                                      msg: "Something went wrong",
                                       toastLength: Toast.LENGTH_LONG,
                                       gravity: ToastGravity.BOTTOM,
                                       timeInSecForIosWeb: 5,
-                                      backgroundColor: Colors.green,
+                                      backgroundColor: Colors.red,
                                       textColor: Colors.white,
                                       fontSize: 16.0);
-                                },
-                              ).catchError((error) {
-                                Fluttertoast.showToast(
-                                    msg: "Something went wrong",
-                                    toastLength: Toast.LENGTH_LONG,
-                                    gravity: ToastGravity.BOTTOM,
-                                    timeInSecForIosWeb: 5,
-                                    backgroundColor: Colors.red,
-                                    textColor: Colors.white,
-                                    fontSize: 16.0);
-                              });
-                            }
-                          })
-                    ]),
+                                });
+                              }
+                            })
+                      ]),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
